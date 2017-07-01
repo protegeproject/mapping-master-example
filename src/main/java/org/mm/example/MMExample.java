1b4bd7debc2467118b754ea375a38379a02572f2
zippyrate <martin@zippyrate.com>
2017-07-01 15:55:48 -0700

    More API calls

--- src/main/java/org/mm/example/MMExample.java
+++ src/main/java/org/mm/example/MMExample.java
@@ -1,10 +1,21 @@
 package org.mm.example;
 
+import org.mm.core.TransformationRule;
+import org.mm.core.settings.ReferenceSettings;
+import org.mm.parser.ASTExpression;
+import org.mm.parser.MappingMasterParser;
+import org.mm.parser.ParseException;
+import org.mm.parser.SimpleNode;
+import org.mm.parser.node.ExpressionNode;
+import org.mm.parser.node.MMExpressionNode;
+import org.mm.renderer.Renderer;
+import org.mm.rendering.Rendering;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.util.Optional;
 
@@ -27,6 +38,19 @@ public class MMExample
         ontologyManager.loadOntologyFromOntologyDocument(owlFile.get()) :
         ontologyManager.createOntology();
 
+      //  public OWLRenderer(OWLOntologySource ontologySource, SpreadSheetDataSource dataSource) {
+
+      Renderer renderer = null;
+      TransformationRule rule = null;
+      String ruleString = rule.getRuleString();
+      MappingMasterParser parser = new MappingMasterParser(new ByteArrayInputStream(ruleString.getBytes()), new ReferenceSettings(), -1);
+      SimpleNode simpleNode = parser.expression();
+      MMExpressionNode ruleNode = new ExpressionNode((ASTExpression) simpleNode).getMMExpressionNode();
+      Optional<? extends Rendering> renderingResult = renderer.render(ruleNode);
+      if (renderingResult.isPresent()) {
+        renderingResult.toString();
+      }
+
 
     } catch (OWLOntologyCreationException e) {
       System.err.println("Error creating OWL ontology: " + e.getMessage());
@@ -34,6 +58,8 @@ public class MMExample
     } catch (RuntimeException e) {
       System.err.println("Error starting application: " + e.getMessage());
       System.exit(-1);
+    } catch (ParseException e) {
+      e.printStackTrace();
     }
   }
 
